9eed6934b693ddd274ffa06107f6f7f4861adcbd
Jose David <deazoft@gmail.com>
2020-02-15 18:39:48 -0500

    Ajustes para phpmyadmin y spring boot en el docker compose

--- src/test/java/com/deazoft/prueba/PruebaApplicationTests.java
+++ src/test/java/com/deazoft/prueba/PruebaApplicationTests.java
@@ -1,13 +1,17 @@
 package com.deazoft.prueba;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 class PruebaApplicationTests {
 
 	@Test
+	
 	void contextLoads() {
 	}
 
 }
+
